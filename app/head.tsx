44fa62a047c02771899e8c0d9cc3a4a827f65132
crushwan <crushwan@gmail.com>
2023-02-09 17:53:44 +0800

    "add verify"

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>ChatGPT Messenger</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/faviconchat.ico" />
+      <meta name="google-site-verification" content="dippTDKwCAq5Ia2-Z_NkJNcGF9VpKwVOV-tMxE9rG9k" />
     </>
   );
 }
